20749db7b38119f4a9e024d593ae23ab5b79734d
erikthegamer1242 <71411196+erikthegamer1242@users.noreply.github.com>
2023-06-15 21:34:14 +0200

    t

--- FPS-TTT/Assets/Scripts/GameHandler.cs
+++ FPS-TTT/Assets/Scripts/GameHandler.cs
@@ -5,34 +5,19 @@ using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using System;
 
 public class GameHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject[] squares = new GameObject[10];
+    private GameObject[] squares = new GameObject[9];
 
-    public GameObject TempHandler;
     public TempHandler tempHandler;
 
     int counter = 0;
-    bool whoIsPlaying = false; // True is X, False is O
-    int squareBattle = -1;
 
     void SetText(GameObject gameObject, string str)
     {
         gameObject.GetComponentInChildren<TextMeshProUGUI>().text = str;
-    }
-
-    void PlayGame(int square)
-    {
-        // Debug.Log(squares[square].GetComponentInChildren<TextMeshProUGUI>().text);
-        if (squares[square].GetComponentInChildren<TextMeshProUGUI>().text == "") 
-        {
-            squareBattle = square;
-            this.gameObject.SetActive(false);
-            TempHandler.SetActive(true);
-        }
     }
 
     void Start()
@@ -42,19 +27,17 @@ public class GameHandler : MonoBehaviour
             string num = (i).ToString();
             squares[i] = GameObject.Find(num);
             SetText(squares[i], "");
-            squares[i].GetComponentInChildren<TextMeshProUGUI>().fontSize = 250;
-            // Debug.Log(num);
-            // Debug.Log(squares[i]);
+            Debug.Log(num);
+            Debug.Log(squares[i]);
         }
-        if (whoIsPlaying) { SetText(squares[9], "On move is: \n X"); }
-        else SetText(squares[9], "On move is: \n O");
-        squares[9].GetComponentInChildren<TextMeshProUGUI>().fontSize = 32;
-        this.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
     }
+
+   
+
     public void TempHideSelf()
     {
         this.gameObject.SetActive(false);
-        TempHandler.SetActive(true);
     }
 
 
@@ -66,61 +49,7 @@ public class GameHandler : MonoBehaviour
 
     private void OnEnable()
     {
-        if (tempHandler.areYouWinningSon)
-        {
-            if (whoIsPlaying) SetText(squares[squareBattle], "X");
-            else SetText(squares[squareBattle], "O");
-        }
-        whoIsPlaying = !whoIsPlaying;
-        // Debug.Log (whoIsPlaying);
-        if (whoIsPlaying) { SetText(squares[9], "On move is: \n X"); }
-        else SetText(squares[9], "On move is: \n O");
-        //SetText(squares[counter], "test please stay " + counter.ToString() + $"(btw we: {tempHandler.areYouWinningSon}");
-        //counter++;
-    }
-
-    public void SquareOne()
-    {
-        PlayGame(0);
-    }
-
-    public void SquareTwo()
-    {
-        PlayGame(1);
-    }
-
-    public void SquareThree()
-    {
-        PlayGame(2);
-    }
-
-    public void SquareFour()
-    {
-        PlayGame(3);
-    }
-
-    public void SquareFive()
-    {
-        PlayGame(4);
-    }
-
-    public void SquareSix()
-    {
-        PlayGame(5);
-    }
-
-    public void SquareSeven()
-    {
-        PlayGame(6);
-    }
-
-    public void SquareEight()
-    {
-        PlayGame(7);
-    }
-
-    public void SquareNine()
-    {
-        PlayGame(8);
+        SetText(squares[counter], "test please stay " + counter.ToString() + $"(btw we: {tempHandler.areYouWinningSon}");
+        counter++;
     }
 }
